5dc7afa4ca9384d1e0a922e4b5be430c69f55746
Alex <sytchevalexander@gmail.com>
2017-09-17 23:17:09 +0200

    Only DB access missing (I hope)

--- DayTasks/app/src/main/java/sytchie/daytasks/MainActivity.java
+++ DayTasks/app/src/main/java/sytchie/daytasks/MainActivity.java
@@ -3,90 +3,151 @@ package sytchie.daytasks;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.*;
 
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.Locale;
+import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
+    private final String[] taskList = {"Morning Sport", "Sport", "Stepper", "Walk", "Read"};
+
     private String date = null, dayStartTime = null, dayEndTime = null;
-    private boolean[] done = new boolean[5];
-    private String[] posThings = new String[3];
+    private CheckBox[] checkBoxes = new CheckBox[5];
+    private Map<String, Boolean> tasks = new HashMap<>();
+    private EditText[] editTexts = new EditText[3];
+    private Map<String, String> posThings = new HashMap<>();
+    private boolean enableSubmit = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        loadDay();
         if (dayStartTime == null) {
             setContentView(R.layout.layout_day_start);
         } else {
             setContentView(R.layout.layout_tasks);
+            loadCheckBoxes();
+            String time = "Day started at " + dayStartTime;
+            TextView textView = (TextView) findViewById(R.id.text_day_start);
+            textView.setText(time);
             startTaskListeners();
         }
-        Arrays.fill(done, false);
     }
 
-    private void startTaskListener(CheckBox checkBox, final int i) {
-        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-                done[i] = isChecked;
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveDay();
+    }
+
+    private void loadDay() {
+        date = new SimpleDateFormat("dd.MM.yyyy", Locale.GERMANY).format(new Date());
+        SharedPreferences sharedPreferences = getSharedPreferences("day_" + date.replaceAll("\\.", "-"), MODE_PRIVATE);
+        //sharedPreferences.edit().clear().apply(); //Debug
+        dayStartTime = sharedPreferences.getString("day_start_time", null);
+        dayEndTime = sharedPreferences.getString("day_end_time", null);
+        for (String task : taskList) {
+            tasks.put(task, Boolean.valueOf(sharedPreferences.getString(task, null)));
+        }
+        for (int i = 1; i <= editTexts.length; i++) {
+            String posThing = "pos_thing_" + i;
+            String posString = sharedPreferences.getString(posThing, null);
+            if (posString == null) {
+                posString = "Positive thing " + i;
             }
-        });
+            posThings.put(posThing, posString);
+        }
+    }
+
+    private void loadCheckBoxes() {
+        checkBoxes[0] = (CheckBox) findViewById(R.id.checkBox);
+        checkBoxes[1] = (CheckBox) findViewById(R.id.checkBox2);
+        checkBoxes[2] = (CheckBox) findViewById(R.id.checkBox3);
+        checkBoxes[3] = (CheckBox) findViewById(R.id.checkBox4);
+        checkBoxes[4] = (CheckBox) findViewById(R.id.checkBox5);
+        int i = 0;
+        for (String task : tasks.keySet()) {
+            boolean tmp = tasks.get(task);
+            checkBoxes[i++].setChecked(tmp);
+        }
+    }
+
+    private void loadEditTexts() {
+        editTexts[0] = (EditText) findViewById(R.id.editText);
+        editTexts[1] = (EditText) findViewById(R.id.editText2);
+        editTexts[2] = (EditText) findViewById(R.id.editText3);
+        for (String posThing : posThings.keySet()) {
+            editTexts[Integer.parseInt(posThing.substring(posThing.length() - 1)) - 1].setText(posThings.get(posThing));
+        }
+        enableSubmit = !(posThings.containsValue(null) ||
+                posThings.containsValue("") ||
+                posThings.containsValue("Positive thing 1") ||
+                posThings.containsValue("Positive thing 2") ||
+                posThings.containsValue("Positive thing 3"));
+    }
+
+    private void saveDay() {
+        SharedPreferences sharedPreferences = getSharedPreferences("day_" + date.replaceAll("\\.", "-"), MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("day_start_time", dayStartTime);
+        editor.putString("day_end_time", dayEndTime);
+        for (String task : tasks.keySet()) {
+            editor.putString(task, String.valueOf(tasks.get(task)));
+        }
+        for (int i = 1; i <= editTexts.length; i++) {
+            String posThing = "pos_thing_" + i;
+            editor.putString(posThing, posThings.get(posThing));
+        }
+        editor.apply();
     }
 
     private void startTaskListeners() {
-        CheckBox checkBox = (CheckBox) findViewById(R.id.checkBox);
-        startTaskListener(checkBox, 0);
-        checkBox = (CheckBox) findViewById(R.id.checkBox2);
-        startTaskListener(checkBox, 1);
-        checkBox = (CheckBox) findViewById(R.id.checkBox3);
-        startTaskListener(checkBox, 2);
-        checkBox = (CheckBox) findViewById(R.id.checkBox4);
-        startTaskListener(checkBox, 3);
-        checkBox = (CheckBox) findViewById(R.id.checkBox5);
-        startTaskListener(checkBox, 4);
-    }
-
-    private void startTextListener(final EditText editText, final int i) {
-        editText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                Button button = (Button) findViewById(R.id.button_submit);
-                button.setEnabled(false);
-                posThings[i] = editText.getText().toString();
-                if (!(Arrays.asList(posThings).contains(null)
-                        || Arrays.asList(posThings).contains("")
-                        || posThings[0].matches("First positive thing")
-                        || posThings[1].matches("Second positive thing")
-                        || posThings[2].matches("Third positive thing"))) {
-                    button.setEnabled(true);
+        int i = 0;
+        for (final String task : tasks.keySet()) {
+            checkBoxes[i++].setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                    tasks.put(task, isChecked);
                 }
-            }
-        });
+            });
+        }
     }
 
     private void startTextListeners() {
-        EditText editText = (EditText) findViewById(R.id.editText);
-        startTextListener(editText, 0);
-        editText = (EditText) findViewById(R.id.editText2);
-        startTextListener(editText, 1);
-        editText = (EditText) findViewById(R.id.editText3);
-        startTextListener(editText, 2);
+        int i = 0;
+        for (final String posThing : posThings.keySet()) {
+            final EditText editText = editTexts[i++];
+            final Button button = (Button) findViewById(R.id.button_submit);
+            button.setEnabled(enableSubmit);
+            editText.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                }
+
+                @Override
+                public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                }
+
+                @Override
+                public void afterTextChanged(Editable editable) {
+                    button.setEnabled(false);
+                    posThings.put(posThing, editText.getText().toString());
+                    if (!(posThings.containsValue(null) ||
+                            posThings.containsValue("") ||
+                            posThings.containsValue("Positive thing 1") ||
+                            posThings.containsValue("Positive thing 2") ||
+                            posThings.containsValue("Positive thing 3"))) {
+                        button.setEnabled(true);
+                    }
+                }
+            });
+        }
     }
 
     private void copyToClipboard(String msg) {
@@ -104,6 +165,7 @@ public class MainActivity extends AppCompatActivity {
         date = new SimpleDateFormat("dd.MM.yyyy", Locale.GERMANY).format(new Date());
         String time = "Day started at " + dayStartTime;
         setContentView(R.layout.layout_tasks);
+        loadCheckBoxes();
         startTaskListeners();
         TextView textView = (TextView) findViewById(R.id.text_day_start);
         textView.setText(time);
@@ -113,6 +175,7 @@ public class MainActivity extends AppCompatActivity {
         dayEndTime = new SimpleDateFormat("HH:mm", Locale.GERMANY).format(new Date());
         String time = "Day ended at " + dayEndTime;
         setContentView(R.layout.layout_day_end);
+        loadEditTexts();
         startTextListeners();
         TextView textView = (TextView) findViewById(R.id.text_day_end);
         textView.setText(time);
@@ -121,17 +184,16 @@ public class MainActivity extends AppCompatActivity {
     public void submit(View view) {
         String summary = "Summary for " + date + ":\n\n" +
                 "Day start at " + dayStartTime + "\n" +
-                "Day end at " + dayEndTime + "\n\n" +
-                "Morning sport: " + done[0] + "\n" +
-                "Sport: " + done[1] + "\n" +
-                "Stepper: " + done[2] + "\n" +
-                "Walk: " + done[3] + "\n" +
-                "Read: " + done[4] + "\n\n" +
-                "Positive thing 1: " + posThings[0] + "\n" +
-                "Positive thing 2: " + posThings[1] + "\n" +
-                "Positive thing 3: " + posThings[2];
+                "Day end at " + dayEndTime + "\n\n";
+        for (String task : tasks.keySet()) {
+            summary += task + ": " + tasks.get(task) + "\n";
+        }
+        summary += "\n";
+        for (String posThing : posThings.keySet()) {
+            summary += "Positive thing " + posThing.substring(posThing.length() - 1) + ": " + posThings.get(posThing) + "\n";
+        }
         copyToClipboard(summary);
-        saveToDB();
+        saveDay();
         setContentView(R.layout.layout_summary);
         TextView textView = (TextView) findViewById(R.id.text_summary);
         textView.setText(summary);
